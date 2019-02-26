d9db963a45d2e38452c7242e161b88e1ca069da9
freek <freek@spatie.be>
2019-02-26 13:19:34 +0100

    wip

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
         <div class="w-1/2 mt-4">
             <form method="POST" action="{{ route('login') }}">
                 @csrf
-                <input id="email" type="email" name="email" placeholder="E-mail" value="{{ old('email') }}" required autofocus class="rounded-sm px-2 h-10 w-full mb-2">
+                <input autocomplete="off" id="email" type="email" name="email" placeholder="E-mail" value="{{ old('email') }}" required autofocus class="rounded-sm px-2 h-10 w-full mb-2">
                 <input id="password" type="password" name="password" placeholder="Password" required class="rounded-sm px-2 h-10 w-full mb-2">
                 <div class="flex items-center justify-between">
                     <button type="submit" class="px-3 h-10 rounded-sm bg-green-gradient text-white font-medium mr-1">
